78c2346be0e51d87169b39be4f8f07e80fc091eb
Ryan <81448533+FagosCORP@users.noreply.github.com>
2025-01-23 11:38:21 -0300

    sliece make usando array interno

---
+++ var-const/sliceMake/sliceMake.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	s := make([] int, 19)
+	s[9] = 10;
+
+	// vai ter 10 elementos mais vai ter 20 posições o array criado
+	// capacidade
+	// tamanho
+	len(s)
+	cap(s)
+
+	s := make([] int, 10, 20)
+	s = append(s, 1,2,3,4,5,56)
+	// ele vai referenciando quando vai crescendo adicionando mais ele vai dobrando de tamanho
+}
